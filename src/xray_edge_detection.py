9b14c39c56a27286290d5f9e4d4dacfcc99059cb
MATTHEWS-NICKLE <matthewsnickle07@gmail.com>
2026-08-30 21:21:15 +0530

    Added X-ray image loading

--- src/xray_edge_detection.py
+++ src/xray_edge_detection.py
@@ -1,27 +1,25 @@
+from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 
 
-def load_xray_image(image_path):
+def load_xray(image_path):
     image = Image.open(image_path).convert("L")
     return np.array(image, dtype=float)
 
 
-def display_image(image):
-    plt.imshow(image, cmap="gray")
-    plt.title("Input X-ray Patch")
-    plt.axis("off")
-    plt.show()
-
-
 if __name__ == "__main__":
+
     image_path = "dataset/xray_patch.png"
 
-    image = load_xray_image(image_path)
+    image = load_xray(image_path)
 
-    print("Image shape:", image.shape)
-    print("Pixel values:")
-    print(image)
+    print("X-ray loaded successfully.")
+    print("Image dimensions:", image.shape)
+    print("Minimum pixel value:", image.min())
+    print("Maximum pixel value:", image.max())
 
-    display_image(image)
+    plt.imshow(image, cmap="gray")
+    plt.title("Original X-ray")
+    plt.axis("off")
+    plt.show()
